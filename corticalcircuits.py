3104c31394748e2bdb282c610d0b5a989bc87053
jakelevi1996 <jakelevi@hotmail.co.uk>
2019-02-26 02:13:23 +0000

    Simulate a LIF neuron with many excitory and inhibitory connections

--- corticalcircuits.py
+++ corticalcircuits.py
@@ -17,11 +17,8 @@ def gen_spike_trains(t_end=2.0, dt=dt, firing_rate=r_X, num_neurons=K):
 
 def estimate_fano(t, spikes, window_time=0.1):
     n_window_steps = np.sum(t < window_time)
-    window_pos_list = np.arange(0, t.size - n_window_steps)
-    counts = np.zeros(window_pos_list.size)
-
-    for pos in window_pos_list:
-        counts[pos] = np.sum(spikes[pos:pos+n_window_steps] > 0)
+    cum_spikes = np.cumsum(spikes > 0)
+    counts = cum_spikes[n_window_steps:] - cum_spikes[:-n_window_steps]
     
     fano = counts.var() / counts.mean()
     return fano
@@ -112,7 +109,7 @@ def q3d():
 
 def q3e():
     w = 4.25
-    t_end = 10.0
+    t_end = 50.0
     t, input_spikes = gen_spike_trains(t_end=t_end)
     summed_inputs = input_spikes.sum(axis=0)
 
@@ -127,15 +124,39 @@ def q3e():
             V[k] = 0.0
             output_spikes[k] = 1.0 / dt
     
-    print(np.sum(output_spikes > 0) / t_end)
+    print("Output firing rate =", np.sum(output_spikes > 0) / t_end)
     fano = estimate_fano(t, output_spikes)
-    print(fano)
+    print("Fano factor ~ {:.5}".format(fano))
 
     plotting.plot_neuron_spikes_many_inputs(
         t, input_spikes, V_above_th, V_th, output_spikes,
         "q3e single LIF neuron many inputs with reset"
     )
 
+def q4():
+    w = 1.5
+    t_end = 50.0
+    t, input_e_spikes = gen_spike_trains(t_end=t_end)
+    summed_e_inputs = input_e_spikes.sum(axis=0)
+    _, input_i_spikes = gen_spike_trains(t_end=t_end)
+    summed_i_inputs = input_i_spikes.sum(axis=0)
+
+    V = np.zeros(t.size)
+    V_above_th = np.zeros(t.size)
+    output_spikes = np.zeros(t.size)
+    
+    for k in range(1, t.size):
+        V[k] = (1.0 - dt / tau) * V[k-1]
+        V[k] += dt * w * summed_e_inputs[k-1] / np.sqrt(K)
+        V[k] -= dt * w * summed_i_inputs[k-1] / np.sqrt(K)
+        V_above_th[k] = V[k]
+        if V[k] > V_th:
+            V[k] = 0.0
+            output_spikes[k] = 1.0 / dt
+    
+    print("Output firing rate =", np.sum(output_spikes > 0) / t_end)
+    fano = estimate_fano(t, output_spikes)
+    print("Fano factor ~ {:.5}".format(fano))
 
 if __name__ == "__main__":
     # q1()
@@ -144,5 +165,7 @@ if __name__ == "__main__":
     # q3a(w=1.0)
     # q3c()
     # q3d()
+    # q3e()
+    # q4()
     
-    for _ in range(5): q3e()
+    for _ in range(5): q4()
